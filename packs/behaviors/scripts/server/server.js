7a99cb9a6fa2a3ee5433e44f6dac6b4cc61f67f8
dzx-dzx <dzx_dzx@hotmail.com>
2019-06-15 00:23:02 +0800

    Ahhhhhhhhhhhhhhhhhhhhhhhhhhhhhh

--- packs/behaviors/scripts/server/server.js
+++ packs/behaviors/scripts/server/server.js
@@ -1,12 +1,16 @@
 var serverSystem = server.registerSystem(0, 0);
 
 var positionArray = new Array(0);
-var block
+var block, blockState, ticking_area
 
 serverSystem.initialize = function () {
     serverSystem.listenForEvent("minecraft:player_placed_block", (eventData) => {
-        display_chat(JSON.stringify(eventData), null, '\t')
-
+        display_chat(JSON.stringify(eventData, null, '\t'))
+        ticking_area = serverSystem.getComponent(eventData.data.player, "minecraft:tick_world").data.ticking_area
+        block = serverSystem.getBlock(ticking_area, eventData.data.block_position)
+        display_chat(JSON.stringify(block, null, '\t'))
+        blockState = serverSystem.getComponent(block, "minecraft:blockstate").data
+        display_chat(JSON.stringify(blockState, null, '\t'))
     })
     serverSystem.listenForEvent("minecraft:entity_created", (eventData) => {
         //display_chat(JSON.stringify(eventData,null,'\t'))
@@ -24,8 +28,11 @@ serverSystem.initialize = function () {
             serverSystem.destroyEntity(entity);
         }
         else if (entity.__identifier__ === "worldedit:execute") {
-            serverSystem.destroyEntity(entity);
+            display_chat("§c NZ is JULAO!")
 
+            display_chat(`/fill ${positionArray[0].x} ${positionArray[0].y} ${positionArray[0].z} ${positionArray[1].x} ${positionArray[1].y} ${positionArray[1].z} ${block.__identifier__.slice("minecraft:".length)}`);
+            serverSystem.executeCommand(`/fill ${positionArray[0].x} ${positionArray[0].y} ${positionArray[0].z} ${positionArray[1].x} ${positionArray[1].y} ${positionArray[1].z} ${block.__identifier__.slice("minecraft:".length)}`, (commandResultData) => { ; });
+            serverSystem.destroyEntity(entity);
         }
     })
 }
