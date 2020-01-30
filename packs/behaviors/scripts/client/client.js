c3392fc1c0c4311ade60bb10d2689b1cd235bbda
dzx-dzx <dzx_dzx@hotmail.com>
2020-01-30 18:21:39 +0800

    Ahhhhhhhhhhhh

--- packs/behaviors/scripts/client/client.js
+++ packs/behaviors/scripts/client/client.js
@@ -1,9 +1,20 @@
 var clientSystem = client.registerSystem(0, 0);
-
+var playerID
 clientSystem.initialize = function () {
-    //clientSystem.listenForEvent("minecraft:client_entered_world", (eventData) => onPlayerEnter())
+    clientSystem.listenForEvent("minecraft:client_entered_world", (eventData) => {playerID = eventData.data.player.id})
+    clientSystem.listenForEvent("worldedit:setPosition", (eventData) => {
+        displayChat(JSON.stringify(eventData, null, '    '))
+    })
 };
 
 clientSystem.update = function () {
 
 };
+
+function displayChat(message) {
+    let eventData = clientSystem.createEventData("minecraft:display_chat_event");
+    if (eventData) {
+        eventData.data.message = message;
+        clientSystem.broadcastEvent("minecraft:display_chat_event", eventData);
+    }
+}
--- packs/behaviors/scripts/server/server.js
+++ packs/behaviors/scripts/server/server.js
@@ -1,40 +1,46 @@
 var serverSystem = server.registerSystem(0, 0);
 
 var positionArray = new Array(0);
-var block, blockState, ticking_area, time = 0
+var block, blockState, ticking_area
 
 serverSystem.initialize = function () {
+
+    serverSystem.registerEventData(
+        "worldedit:setPosition",
+        { blockPosition: { x: undefined, y: undefined, z: undefined }, playerID: undefined }
+    )
+
     serverSystem.listenForEvent("minecraft:player_placed_block", (eventData) => {
-        display_chat(JSON.stringify(eventData, null, '\t'))
+        displayChat(JSON.stringify(eventData, null, '    '))
 
         ticking_area = serverSystem.getComponent(eventData.data.player, "minecraft:tick_world").data.ticking_area
         block = serverSystem.getBlock(ticking_area, eventData.data.block_position)
 
-        display_chat(JSON.stringify(block, null, '\t'))
+        displayChat(JSON.stringify(block, null, '    '))
 
         blockState = serverSystem.getComponent(block, "minecraft:blockstate").data
 
-        display_chat(JSON.stringify(blockState, null, '\t'))
+        displayChat(JSON.stringify(blockState, null, '    '))
     })
     serverSystem.listenForEvent("minecraft:entity_created", (eventData) => {
-        //display_chat(JSON.stringify(eventData,null,'\t'))
+        //displayChat(JSON.stringify(eventData,null,'    '))
         var entity = eventData.data.entity;
         if (entity.__identifier__ === "worldedit:select") {
             var position = serverSystem.getComponent(entity, "minecraft:position").data;
 
-            display_chat(`\nSelecting position:\nx:${position.x}\ny:${position.y}\nz:${position.z}`);
+            displayChat(`\nSelecting position:\nx:${position.x}\ny:${position.y}\nz:${position.z}`);
 
             positionArray.push(position);
             if (positionArray.length >= 3) {
-                display_chat("\nWarning:Positions exceeded.The first position is ignored.");
+                displayChat("\nWarning:Positions exceeded.The first position is ignored.");
                 positionArray.shift();
             }
-            serverSystem.destroyEntity(entity);
+            //serverSystem.destroyEntity(entity);
         }
         else if (entity.__identifier__ === "worldedit:execute") {
-            display_chat("§b NZ is JULAO!")
+            displayChat("§b NZ is JULAO!")
 
-            //display_chat(`/fill ${positionArray[0].x} ${positionArray[0].y} ${positionArray[0].z} ${positionArray[1].x} ${positionArray[1].y} ${positionArray[1].z} ${block.__identifier__.slice("minecraft:".length)}`);
+            //displayChat(`/fill ${positionArray[0].x} ${positionArray[0].y} ${positionArray[0].z} ${positionArray[1].x} ${positionArray[1].y} ${positionArray[1].z} ${block.__identifier__.slice("minecraft:".length)}`);
             //serverSystem.executeCommand(`/fill ${positionArray[0].x} ${positionArray[0].y} ${positionArray[0].z} ${positionArray[1].x} ${positionArray[1].y} ${positionArray[1].z} ${block.__identifier__.slice("minecraft:".length)}`, (commandResultData) => { ; });
             var minPosition = {
                 x: Math.min(positionArray[0].x, positionArray[1].x),
@@ -47,36 +53,58 @@ serverSystem.initialize = function () {
                 z: Math.max(positionArray[0].z, positionArray[1].z)
             }
 
-            display_chat(minPosition.x)
-            display_chat(minPosition.y)
-            display_chat(minPosition.z)
-            display_chat(maxPosition.x)
-            display_chat(maxPosition.y)
-            display_chat(maxPosition.z)
+            displayChat(minPosition.x)
+            displayChat(minPosition.y)
+            displayChat(minPosition.z)
+            displayChat(maxPosition.x)
+            displayChat(maxPosition.y)
+            displayChat(maxPosition.z)
 
             for (var x = minPosition.x; x <= maxPosition.x; x++) {
                 for (var y = minPosition.y; y <= maxPosition.y; y++) {
                     for (var z = minPosition.z; z <= maxPosition.z; z++) {
-                        display_chat("Position:")
-                        display_chat(x)
-                        display_chat(y)
-                        display_chat(z)
-                        generate(x,y,z)
+                        displayChat("Position:")
+                        displayChat(x)
+                        displayChat(y)
+                        displayChat(z)
+                        generate(x, y, z)
                     }
                 }
             }
 
 
-            serverSystem.destroyEntity(entity);
+            //serverSystem.destroyEntity(entity);
         }
+    })
+
+    serverSystem.listenForEvent("minecraft:block_interacted_with", (eventData) => {
+        displayChat("Event \"minecraft:block_interacted_with\" fired:\n" + JSON.stringify(eventData, null, '    '))
+        //TODO:Verify whether the player is permitted to use this addon.
+        let setPositionEventData = serverSystem.createEventData("worldedit:setPosition")
+        setPositionEventData.data.playerID = eventData.data.player.id
+        setPositionEventData.data.blockPosition = eventData.data.block_position
+        serverSystem.broadcastEvent("worldedit:setPosition", setPositionEventData)
+        displayChat(
+            JSON.stringify(
+                serverSystem.getComponent(eventData.data.player, "minecraft:hand_container"),
+                null,
+                '    '
+            )
+        )
+        displayChat(
+            JSON.stringify(
+                serverSystem.getComponent(eventData.data.player, "minecraft:inventory_container"),
+                null,
+                '    '
+            )
+        )
     })
 }
 
 serverSystem.update = function () {
-    time++
 };
 
-function display_chat(message) {
+function displayChat(message) {
     let eventData = serverSystem.createEventData("minecraft:display_chat_event");
     if (eventData) {
         eventData.data.message = message;
@@ -87,15 +115,15 @@ function display_chat(message) {
 function generate(x, y, z) {
     serverSystem.executeCommand(`/setblock ${x} ${y} ${z} ${block.__identifier__.slice("minecraft:".length)}`, (commandResultData) => {
 
-        display_chat(JSON.stringify(commandResultData, null, '\t'));
-        display_chat("Position now:")
-        display_chat(x)
-        display_chat(y)
-        display_chat(z)
+        displayChat(JSON.stringify(commandResultData, null, '    '));
+        displayChat("Position now:")
+        displayChat(x)
+        displayChat(y)
+        displayChat(z)
 
         var targerBlock = serverSystem.getBlock(ticking_area, x, y, z)
 
-        display_chat(JSON.stringify(targerBlock, null, '\t'))
+        displayChat(JSON.stringify(targerBlock, null, '    '))
 
         var targetBlockStateComponent = serverSystem.getComponent(targerBlock, "minecraft:blockstate")
         targetBlockStateComponent.data = blockState
